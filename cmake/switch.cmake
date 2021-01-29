42d609eb44dab7d4829d1d098ef1e5e9dc8f6e89
uyjulian <uyjulian@gmail.com>
2021-01-29 13:25:51 -0600

    Remove CMake toolchain patch shim and use original path instead

--- cmake/switch.cmake
+++
@@ -1,6 +0,0 @@
-include($ENV{DEVKITPRO}/switch.cmake)
-
-# Workaround for compilation / linking test failure
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fPIE")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fPIE")
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} -fPIE")
